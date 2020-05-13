32110ecfbc400aa662f41f155359be9a01cdd95e
Kira Marks <serafita22@gmail.com>
2020-05-13 21:58:11 +0300

    Updates

---
+++ precache-manifest.924c39ae776ed1300f48a1162bf10653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73bb60f3d1bef8b6ed3333e072344fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd0cd4f31e5222d0ddc7",
+    "url": "./static/css/main.8d31dcd1.chunk.css"
+  },
+  {
+    "revision": "e17607bf7d37447b34ef",
+    "url": "./static/js/2.dc7ec307.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dc7ec307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0cd4f31e5222d0ddc7",
+    "url": "./static/js/main.a50b4c79.chunk.js"
+  },
+  {
+    "revision": "55ecf25ea8d2bbd87954",
+    "url": "./static/js/runtime-main.80c0d849.js"
+  }
+]);
